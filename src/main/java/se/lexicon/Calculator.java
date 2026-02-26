93e7c6a7cb80b4d25f42ee46503412071ca5506e
Jayani Athukorala <sandunkauwu@gmail.com>
2026-02-26 11:39:34 +0100

    Add number buttons and function buttons to cal ui

--- src/main/java/se/lexicon/Calculator.java
+++ src/main/java/se/lexicon/Calculator.java
@@ -29,8 +29,92 @@ public class Calculator implements ActionListener {
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(420, 550);
         frame.setLayout(null);
+
+        //TextField to Display Inputs
+        textField = new JTextField();
+        textField.setBounds(50, 25, 300, 50);
+        textField.setFont(myFont);
+        //Fals edits
+        textField.setEditable(false);
+
+        //Buttons
+        addButton = new JButton("+");
+        subButton = new JButton("-");
+        mulButton = new JButton("*");
+        divButton = new JButton("/");
+        decButton = new JButton(".");
+        equButton = new JButton("=");
+        delButton = new JButton("Delete");
+        clrButton = new JButton("Clear");
+
+        //Add above buttons to functionButtons array
+        functionButtons[0] = addButton;
+        functionButtons[1] = subButton;
+        functionButtons[2] = mulButton;
+        functionButtons[3] = divButton;
+        functionButtons[4] = decButton;
+        functionButtons[5] = equButton;
+        functionButtons[6] = delButton;
+        functionButtons[7] = clrButton;
+
+        //Set action listeners to function buttons
+        for (int i = 0; i <= 7; i++ ){
+            functionButtons[i].addActionListener(this);
+            functionButtons[i].setFont(myFont);
+            functionButtons[i].setFocusable(false);
+        }
+
+        //Set action listeners to number buttons
+        for (int i = 0; i <= 9; i++ ){
+            numberButtons[i] = new JButton(String.valueOf(i));
+            numberButtons[i].addActionListener(this);
+            numberButtons[i].setFont(myFont);
+            numberButtons[i].setFocusable(false);
+        }
+
+        //Delete button and Clear button in the bottom of the panel
+        delButton.setBounds(50, 430, 145, 50);
+        clrButton.setBounds(205, 430, 145, 50);
+
+        //Create a new panel
+        panel = new JPanel();
+        panel.setBounds(50, 100, 300, 300);
+        panel.setLayout(new GridLayout(4, 4, 10, 10));
+
+        //Add number buttons and other function buttons to the new panel
+        //Fist raw
+        panel.add(numberButtons[1]);
+        panel.add(numberButtons[2]);
+        panel.add(numberButtons[3]);
+        panel.add(addButton);
+
+        //Second raw
+        panel.add(numberButtons[4]);
+        panel.add(numberButtons[5]);
+        panel.add(numberButtons[6]);
+        panel.add(subButton);
+
+        //Third raw
+        panel.add(numberButtons[7]);
+        panel.add(numberButtons[8]);
+        panel.add(numberButtons[9]);
+        panel.add(mulButton);
+
+        //Fourth raw
+        panel.add(decButton);
+        panel.add(numberButtons[0]);
+        panel.add(equButton);
+        panel.add(divButton);
+
+        // Add new panel to the frame
+        frame.add(panel);
+        frame.add(delButton);
+        frame.add(clrButton);
+        //Add textField to the frame
+        frame.add(textField);
         frame.setVisible(true);
     }
+
     static void main(String[] args) {
 
         //Create an instance of Calculator Class
